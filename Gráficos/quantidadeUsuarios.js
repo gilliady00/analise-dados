9368103be06685b3b6790c728a722e1c939f7c49
gilliady00 <126722329+gilliady00@users.noreply.github.com>
2024-09-26 15:14:52 -0300

    dfgdfg

--- Gráficos/quantidadeUsuarios.js
+++ Gráficos/quantidadeUsuarios.js
@@ -57,3 +57,64 @@ async function quantidadeUsuarios() {
 }
 
 quantidadeUsuarios()
+
+
+import { getCSS, tickConfig } from "./common.js "
+
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = ["Branca","Amarela", "Preta", "Parda", "Indígena"]
+    const quantidadeUsuarios = ["88252121","850130", "20656458", "92083286", "1227642"]
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeUsuarios,
+            type: 'bar',
+            marker: {
+                color: getCSS('--cor-barra')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--cor-de-fundo'),
+        paper_bgcolor: getCSS('--cor-de-fundo'),
+        title: {
+            text: 'População por cor ou raça',
+            font: {
+                color: getCSS('--cor-principal'),
+                family: getCSS('--fonte'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Cor ou raça',
+                font: {
+                    color: getCSS('--cor-secundaria')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Milhões de pessoas',
+                font: {
+                    color: getCSS('--cor-secundaria')
+                }
+            }
+
+        }
+
+    }
+    const grafico = document.createElement('div')
+    grafico.className - 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+
+}
+
+quantidadeUsuarios()
